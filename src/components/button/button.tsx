eae9f39230c369023655a59d7e59a777e48f955e
muhammeddeniz <muhammeddeniz024@gmail.com>
2020-08-06 16:16:00 +0300

    button components

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,34 +5,44 @@ import "./button.scss";
 
 type IProps = JSX.IntrinsicElements["button"] & {
   primary?: boolean;
-  secondary?: boolean;
-  outline?: boolean;
+  success?: boolean;
+  warning?: boolean;
+  danger?: boolean;
+  info?: boolean;
   dark?: boolean;
+  light?: boolean;
 };
 
 const Button: React.FC<IProps> = ({
   children,
   className,
   primary,
-  secondary,
-  outline,
+  success,
+  warning,
+  danger,
+  info,
   dark,
+  light,
+  ...rest
 }) => {
   const btnClass = cx(
     "btn",
     {
       "btn-primary": primary,
-      "btn-secondary": secondary,
-      "btn-outline": outline,
+      "btn-success": success,
+      "btn-warning": warning,
+      "btn-danger": danger,
+      "btn-info": info,
       "btn-dark": dark,
+      "btn-light": light,
     },
     className
   );
 
   return (
-    <div>
-      <button className={btnClass}>{children}</button>
-    </div>
+    <button className={btnClass} {...rest}>
+      {children}
+    </button>
   );
 };
 
--- src/storybook/button.stories.js
+++ src/storybook/button.stories.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Button } from "../components";
 export default { title: "Button" };
 
-export const Primary = () => <Button primary>primary</Button>;
+export const Primary = () => <Button primary>Button</Button>;
 
-export const Secondary = () => <Button secondary>Secondary</Button>;
+export const Succes = () => <Button success>Button</Button>;
 
-export const Outline = () => <Button outline>Outline</Button>;
+export const Warning = () => <Button warning>Button</Button>;
 
-export const Dark = () => <Button dark>Dark</Button>;
+export const Danger = () => <Button danger>Button</Button>;
+
+export const Info = () => <Button info>Button</Button>;
+
+export const Dark = () => <Button dark>Button</Button>;
+
+export const light = () => <Button light>Button</Button>;
